d2821f1a1da96355740b1a0cdc4c83e101b8e956
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 22:46:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 
 import streamlit as st
-import SessionState as ss
 
 st.title('Bienvenido a Sexuapp')
-button1 = st.empty()
-text1 = st.empty()
-button2 = st.empty()
-text2 = st.empty()
-
-ss = SessionState.get(button1 = False)
-
-if button1.button('1') :
-    ss.button1 = True
-
-if ss.button1:
-    text1.write('you clicked the first button')
-    if button2.button('2'):
-        text2.write('you clicked the second button')
-     
+option = st.selectbox(
+     'Cual es tu genero Biologico?',
+     ('hombre', mujer'))
+if hombre
+      option = st.selectbox(
+     'haz presenatdo alguno de los siguientes?',
+     ('ardor', picazon'))
                    
 
