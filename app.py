b95e2e06bbdd1ecc84526bb0252cebf72bcbecb2
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 01:47:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-cb1 = st.checkbox('a')
-
-empty = st.empty()
-empty.checkbox('b')
-
-if cb1:
-    empty.checkbox('b', True)
-Or
-
-import streamlit as st
-
-button = st.button('a')
-
-empty = st.empty()
-empty.checkbox('c')
-
-if button:
-    empty.checkbox('c', True)
+st_btn_select import st_btn_select selection = st_btn_select(('option 1', 'option 2', 'option 3'))
 
 	
 
