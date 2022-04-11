7e0aa375a3f4ccdffe05251ab434a9fa9507742e
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 23:42:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title('Bienvenido a Sexuapp')
 
 st.write('Selecciona tu sexo')
-page_names = ['hombre', 'mujer']
-page = st.ratio('selecciona un sexo',page_names)
-if page = 'hombre':
+page_names=['hombre', 'mujer']
+page=st.ratio('selecciona un sexo',page_names)
+if page='hombre':
 	st.button('good?')
 
 	
