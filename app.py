ffe269d6ddc4082e0d0ecfeb7d1fdedd1a17109f
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 03:17:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-option = st.selectbox(
-     'cual es tu sexo biologico?',
-     ('hombre', 'mujer'))
-option = st.selectbox(
-     'sintomas?',
-     ('hinchazon', 'picazon'))
-# Input fields
 a = st.selectbox(
      'cual es tu sexo biologico?',
      ('hombre', 'mujer'))
-b = st.st.selectbox(
+b = st.selectbox(
      'sintomas?',
      ('hinchazon', 'picazon'))
+# Input fields
+if st.button("submit"):
+     result = herpes
 
 
   
