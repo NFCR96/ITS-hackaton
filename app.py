b04a70c44c8247d6a6ea2b8cc9f8aef539b893c2
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 00:25:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
+if 'my_button' not in st.session_state:
+    st.session_state.my_button = True
+    # Streamlit will raise an Exception on trying to set the state of button
 
-if "celsius" not in st.session_state:
-    # set the initial default value of the slider widget
-    st.session_state.celsius = 50.0
-
-st.slider(
-    "Temperature in Celsius",
-    min_value=-100.0,
-    max_value=100.0,
-    key="celsius"
-)
-
-# This will get the value of the slider widget
-st.write(st.session_state.celsius)
-
+st.button('Submit', key='my_button')
 
 	
 
