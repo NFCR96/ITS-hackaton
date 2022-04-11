2493da65c5ca7c3f1c04c740f682cc0a5bb0b07c
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 03:05:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ st.title('Bienvenido a Sexuapp')
 option = st.selectbox(
      'cual es tu sexo biologico?',
      ('hombre', 'mujer'))
-
-if hombre:
-     option = st.selectbox(
+option = st.selectbox(
      'sintomas?',
-     ('comezon', 'hinchazon'))
+     ('hinchazon', 'picazon'))
+
                    
 
