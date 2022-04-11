a079d21f0406c972cd147d92925d7a337e699834
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 23:23:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-st_btn_select import st_btn_select selection = st_btn_select(('option 1', 'option 2', 'option 3'))
+st.multiselect('Multiselect', [hombre,mujer])
 	
 
 
