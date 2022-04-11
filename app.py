caccf578107f72b5f0919a1cf7b94df14df1fc4d
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 01:49:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-st_btn_select import st_btn_select selection = st_btn_select(('option 1', 'option 2', 'option 3'))
+import SessionState
+
+button1 = st.empty()
+text1 = st.empty()
+button2 = st.empty()
+text2 = st.empty()
+
+ss = SessionState.get(button1 = False)
+
+if button1.button('1') :
+    ss.button1 = True
+
+if ss.button1:
+    text1.write('you clicked the first button')
+    if button2.button('2'):
+        text2.write('you clicked the second button')
 
 	
 
