cd2d712142e875c2616ddc9ddb2dce8356ba08b1
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 23:31:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ st.title('Bienvenido a Sexuapp')
 st.write('Selecciona tu sexo')
 
 result = st.selectbox('Select', [Hombre,Mujer])
-st.selectbox('Select', [Hombre,Mujer])
+if hombre:
+	st.selectbox('Select', [Hombre,Mujer])
+
 	
 
 
