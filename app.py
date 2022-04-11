5fda5bfcb8667951bd83d24a3e5468bc0aea89b0
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 02:00:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-import SessionState
+button1 = st.button('Check 1')
 
-button1 = st.empty()
-text1 = st.empty()
-button2 = st.empty()
-text2 = st.empty()
+if st.session_state.get('button') != True:
 
-ss = SessionState.get(button1 = False)
+    st.session_state['button'] = button1
 
-if button1.button('1') :
-    ss.button1 = True
+if st.session_state['button'] == True:
 
-if ss.button1:
-    text1.write('you clicked the first button')
-    if button2.button('2'):
-        text2.write('you clicked the second button')
+    st.write("button1 is True")
+
+    if st.button('Check 2'):
+
+        st.write("Hello, it's working")
+
+        st.session_state['button'] = False
+
+        st.checkbox('Reload')
 
 	
 
