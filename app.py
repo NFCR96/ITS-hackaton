16ee30477025ba2d6d1847de69e9d3349affbcdb
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 03:08:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ option = st.selectbox(
 option = st.selectbox(
      'sintomas?',
      ('hinchazon', 'picazon'))
+# Input fields
+a = st.number_input("First value", 1, 1000)
+b = st.number_input("Second value", 1, 1000)
+
+# Perform an operation, and save its result
+if st.button("Compute value"):
+    result = a * b
+    st.experimental_set_query_params(my_saved_result=result)  # Save value
+
+# Retrieve app state
+app_state = st.experimental_get_query_params()  
+
+# Display saved result if it exist
+if "my_saved_result" in app_state:
+    saved_result = app_state["my_saved_result"][0]
+    st.write("Here is your result", saved_result)
+else:
+    st.write("No result to display, compute a value first.")
 
                    
 
