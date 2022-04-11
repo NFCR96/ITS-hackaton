9f00be219ff27be7dfc70fae595b55f9b9e2074c
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 03:12:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,17 @@ option = st.selectbox(
      'sintomas?',
      ('hinchazon', 'picazon'))
 # Input fields
-a = st.number_input("First value", 1, 1000)
-b = st.number_input("Second value", 1, 1000)
+a = st.selectbox(
+     'cual es tu sexo biologico?',
+     ('hombre', 'mujer'))
+b = st.st.selectbox(
+     'sintomas?',
+     ('hinchazon', 'picazon'))
 
 # Perform an operation, and save its result
-if st.button("Compute value"):
-    result = a * b
-    st.experimental_set_query_params(my_saved_result=result)  # Save value
-
-# Retrieve app state
-app_state = st.experimental_get_query_params()  
-
-# Display saved result if it exist
-if "my_saved_result" in app_state:
-    saved_result = app_state["my_saved_result"][0]
-    st.write("Here is your result", saved_result)
-else:
-    st.write("No result to display, compute a value first.")
+if st.selectbox("hombre"):
+  
+    
 
                    
 
