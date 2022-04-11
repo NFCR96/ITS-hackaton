2780fb6597b8ca766f25e8aa43222ae27cedad20
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 22:50:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
+if st.session_state.get('step') is None:
+    st.session_state['step'] = 0
+# the number is my 'configuration'
+if st.session_state.get('number') is None:
+    st.session_state['number'] = 0
+  
+  
+if st.button('start configuration'):
+    st.session_state['step'] = 1
+  
+if st.session_state['step'] == 1:
+    with st.form('my form'):
+        st.session_state['number'] = st.number_input('choose a number', 1, 13)
+        if st.form_submit_button("save configuration"):            
+            st.session_state['step'] = 2
+            st.experimental_rerun()  # form should not be shown after clicking 'save' 
+  
+  
+if st.session_state['step'] == 2:
+    st.write('Your configuration is:')
+    st.write(f'number: {st.session_state["number"]}')
+
 
 
                    
