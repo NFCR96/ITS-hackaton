117f037c85820b1500c9b5f244fed55b6a3cc241
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 21:27:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title('Bienvenido a Sexuapp')
 option = st.selectbox(
      'Cual es tu sexo biologico',
      ('hombre', 'mujer',))
-st.form_submit_button("hombre")
+st.button('you selected',hombre)
      
 
