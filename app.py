e4c16edaa2877ec69b7f6fb3bcca9535cc45b235
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 03:46:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-a = st.selectbox(
-     'cual es tu sexo biologico?',
-     ('hombre', 'mujer'))
-if st.button('hombre'):
+page_names = '['mujer', 'hombre']
+page = st.ratio('Sexo', page_names)
+st.write("**a conitnuacion selecciona lo siguiente:**",page)
+
+if page = 'checkbox':
+     st.subheader('welcome to sexuapp!')
+     st.write("nice to see you! :wave:")
+     check = st.checkbox("click here")
+     st.write('state of the checkbox:', check)
+if check:
+     nested_btn = st.button("button nested in checkbox")
+     
+     if nested_btn:
+          st.write(":cake:"*20)
+              
+
+
 
      
 
