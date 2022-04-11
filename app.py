9c12e356840ed379bbb2a051f1edee7d781cf681
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 22:38:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 
 import streamlit as st
-st.title('Bienvenido a Sexuapp')
+import SessionState
 
-genre = st.radio(
-     "cual es tu sexo biologico?",
-     ('Mujer', 'Hombre'))
-if biologico == 'Hombre':
-     st.button('haz presentado alguno de estos sintomas?')
+st.title('Bienvenido a Sexuapp')
+button1 = st.empty()
+text1 = st.empty()
+button2 = st.empty()
+text2 = st.empty()
+
+ss = SessionState.get(button1 = False)
+
+if button1.button('1') :
+    ss.button1 = True
+
+if ss.button1:
+    text1.write('you clicked the first button')
+    if button2.button('2'):
+        text2.write('you clicked the second button')
      
                    
 
