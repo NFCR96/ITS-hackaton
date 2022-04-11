93a553628939807e557bc83da25287d7d78db11c
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 00:02:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,18 @@ import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
 
-st.write('Selecciona tu sexo')
-d=a+b
-  
-  
-a=str(a).replace('0','')
-b=str(b).replace('0','')
-  
-c=int(a)+int(b)
-  
-if c==d:
-    print('Yes')
-else:
-    print('No')
+import pandas as pd
 
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
 	
 
 
