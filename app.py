61450bd6e10f3d2431753b1b0fbd53464d8e9829
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 22:58:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-# Using the "with" syntax
-with st.form(key='ITS'):
+form = st.form(key='my-form')
+name = form.text_input('Enter your name')
+submit = form.form_submit_button('Submit')
+
+st.write('Press submit to have your name printed below')
+
+if submit:
+    st.write(f'hello {name}')
 	
 
 
