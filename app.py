e529073a456194e7a75f15d9c19db1b5bc8adbf9
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 02:33:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,22 @@ if st.session_state['button'] == True:
 
         st.checkbox('Hinchazon', 'dolor', 'llagas', 'berrugas')
 	
+	
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+if st.session_state.get('multiselect') != True:
+	 st.session_state['multiselect'] = options
+		if st.session_state['multiselect'] == True:
+			st.write("options is True")
+			 if st.multiselect('Hombre', 'mujer'):
 
+        st.write("Hello, it's working")
+
+        st.session_state['multiselect'] = False
+
+        st.multiselect('Hinchazon', 'dolor', 'llagas', 'berrugas')
 	
 
 
