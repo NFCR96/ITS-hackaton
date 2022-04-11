d4ae3ffbac2d5f9df0de3f5578998042116ab129
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 23:14:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-# Streamlit runs from top to bottom on every iteraction so
-# we check if `count` has already been initialized in st.session_state.
-
-# If no, then initialize count to 0
-# If count is already initialized, don't do anything
-if 'count' not in st.session_state:
-	st.session_state.count = 0
-
-# Create a button which will increment the counter
-increment = st.button('Increment')
-if increment:
-    st.session_state.count += 1
-
-# A button to decrement the counter
-decrement = st.button('Decrement')
-if decrement:
-    st.session_state.count -= 1
-
-st.write('Count = ', st.session_state.count)
+st_btn_select import st_btn_select selection = st_btn_select(('option 1', 'option 2', 'option 3'))
 	
 
 
