925bcc7430c54359326c268cab11a6ef6e5f9bcc
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-11 03:24:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ st.title('Bienvenido a Sexuapp')
 a = st.selectbox(
      'cual es tu sexo biologico?',
      ('hombre', 'mujer'))
-b = st.selectbox(
-     'sintomas?',
-     ('hinchazon', 'picazon'))
-# Input fields
-if st.button("submit"):
-     result = a+b
+if st.selectbox('hombre')
+
      
 
 
