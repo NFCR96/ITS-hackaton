7f1e53b69b2f50341e3efb91a29d1b65da885fec
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-10 22:57:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 st.title('Bienvenido a Sexuapp')
 # Using the "with" syntax
 with st.form(key='ITS'):
-	button = st.button('sexo al nacer?')
-	submit_button = st.form_submit_button(label='Submit')
+	
 
 
 
