8a5ba367bf6223db1cbd4e94460ca136a6876550
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-14 00:40:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,37 +2,19 @@ import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
 
-import numpy as np
-import session_state
-from streamlit.ScriptRunner import RerunException
-from streamlit.ScriptRequestQueue import RerunData
-
-state = session_state.get(question_number=0)
-
-@st.cache
-def get_question(question_number):
-    arr = np.random.randint(0, 100, 2)
-    q = f"{arr[0]} * {arr[1]}"
-    ans = arr[0]*arr[1]
-    choices = ["Please select an answer", ans, ans-1, ans+1, ans+2]
-    return arr, q, ans, choices
-
-arr, q, ans, choices = get_question(state.question_number)
-
-st.text(f"Solve: {q}")
-a = st.selectbox('Answer:', choices)
-
-if a != "Please select an answer":
-    st.write(f"You chose {a}")
-    if (ans == a):
-        st.write("Correct!")
-    else:
-        st.write(f"Wrong!, the correct answer is {ans}")
-            
-if st.button('Next question'):
-    state.question_number += 1
-    raise RerunException(RerunData(widget_state=None))
-
+fill_in_the_blanks = {'questions': ['question1','question2'], 'answers': ['answer1','answer2']}
+ans = []
+mark = 0
+with st.form(key = id_generator()):
+	for i,quest in enumerate(fill_in_the_blanks["questions"]):
+		st.write(i+1,quest)
+		x = st.text_input("Enter your answer here",key=id_generator())
+		ans.append(x)
+	submitted = st.form_submit_button(label='Submit')
+	if submitted:
+		# if answers are correct then
+			mark = mark+1
+	st.success("Test Score - "+str(mark))
 
 
      
