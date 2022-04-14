9c002996cd75f8b4080228b02c12fe3625fa432b
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-14 00:36:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
-page_names = ['mujer', 'hombre']
-page = st.ratio('Sexo', page_names)
-st.write("**a conitnuacion selecciona lo siguiente:**",page)
+import numpy as np
+import session_state
+from streamlit.ScriptRunner import RerunException
+from streamlit.ScriptRequestQueue import RerunData
 
-if page = 'mujer':
-     st.subheader('welcome to sexuapp!')
-     st.write("nice to see you! :wave:")
-     check = st.checkbox("click here")
-     st.write('state of the checkbox:', check)
-if check:
-     nested_btn = st.button("button nested in checkbox")
-     
-     if nested_btn:
-          st.write(":cake:"*20)
-              
+state = session_state.get(question_number=0)
+
+@st.cache
+def get_question(question_number):
+    arr = np.random.randint(0, 100, 2)
+    q = f"{arr[0]} * {arr[1]}"
+    ans = arr[0]*arr[1]
+    choices = ["Please select an answer", ans, ans-1, ans+1, ans+2]
+    return arr, q, ans, choices
+
+arr, q, ans, choices = get_question(state.question_number)
+
+st.text(f"Solve: {q}")
+a = st.selectbox('Answer:', choices)
+
+if a != "Please select an answer":
+    st.write(f"You chose {a}")
+    if (ans == a):
+        st.write("Correct!")
+    else:
+        st.write(f"Wrong!, the correct answer is {ans}")
+            
+if st.button('Next question'):
+    state.question_number += 1
+    raise RerunException(RerunData(widget_state=None))
 
 
 
