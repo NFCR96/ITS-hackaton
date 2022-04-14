da9a871797bef67f6160693c797c6cd9748545e1
NFCR96 <102831877+NFCR96@users.noreply.github.com>
2022-04-14 00:59:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
 import streamlit as st
 
 st.title('Bienvenido a Sexuapp')
+if 'num' not in st.session_state:
+    st.session_state.num = 0
 
-import numpy as np
 
-arr = np.random.randint(0, 100, 2)
-q = f"{arr[0]} * {arr[1]}"
-ans = arr[0]*arr[1]
-choices = [0, ans, ans-1, ans+1, ans+2]
+choices1 = ['no answer', 'manila', 'tokyo', 'bangkok']
+choices2 = ['no answer', 'thailand', 'japan', 'philippines']
 
-st.text(f"Solve: {q}")
-a = st.selectbox('Answer:', choices)
-st.write(f"You chose {a}")
+qs1 = [('What is the capital of Japan', choices1),
+    ('What is the capital of Philippines', choices1),
+    ('What is the capital of Thailand', choices1)]
+qs2 = [('What country has the highest life expectancy?', choices2),
+    ('What country has the highest population?', choices2),
+    ('What country has the highest oil deposits?', choices2)]
 
-if (ans == int(a)):
-    st.write("Correct!")
-else:
-    st.write(f"Wrong!, the correct answer is {ans}")
-            
-st.button('Rerun')
+
+def main():
+    for _, _ in zip(qs1, qs2): 
+        placeholder = st.empty()
+        num = st.session_state.num
+        with placeholder.form(key=str(num)):
+            st.radio(qs1[num][0], key=num+1, options=qs1[num][1])
+            st.radio(qs2[num][0], key=num+1, options=qs2[num][1])          
+                      
+            if st.form_submit_button():
+                st.session_state.num += 1
+                if st.session_state.num >= 3:
+                    st.session_state.num = 0 
+                placeholder.empty()
+            else:
+                st.stop()
+
+
+main()
+
 
   
     
